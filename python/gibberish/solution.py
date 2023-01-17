6ba599bbbc993981a6e571218f392a5f23f1d094
Reylian Anareka <reyliananareka@gmail.com>
2023-01-17 13:29:17 +0700

    commit solution for gibberish problem in python

---
+++ python/gibberish/solution.py
@@ -0,0 +1,30 @@
+"""
+
+The problem is to make a program that print the number of each letter from the string 
+Example :
+
+input :
+asdfasdasdwaw
+
+output :
+    "a":4,
+    "s":3,
+    "d":3,
+    "f":1,
+    "w":2
+    
+"""
+
+def decompose(word):
+    wordDict = {}
+
+    for letter in word :
+        try :
+            wordDict[letter] += 1
+        except KeyError:
+            wordDict[letter] = 1
+    
+    for key, value in wordDict.items():
+        print(key+" : "+str(value))
+
+decompose("nainuainaniaieueo")
